f7900f6a8ac3361b2b7d28d25546dc94ed9a1402
mu <59917266+4eUeP@users.noreply.github.com>
2020-09-19 20:39:23 +0800

    Server: retry while reading from stdout

--- app/server.hs
+++ app/server.hs
@@ -6,7 +6,7 @@
 module Main (main) where
 
 import           Control.Concurrent            (forkIO)
-import           Control.Monad                 (void)
+import           Control.Monad                 (void, when)
 import           Control.Monad.IO.Class        (liftIO)
 import           Control.Monad.Reader          (ask)
 import           Data.ByteString               (ByteString)
@@ -18,11 +18,12 @@ import           Data.Yaml.Config              (loadYamlSettingsArgs, useEnv)
 import           Network.Socket                (Socket)
 import           Network.Socket.ByteString     (recv, sendAll)
 import           System.IO                     (Handle)
+import qualified System.IO                     as IO
 
 import           LSP.Network                   (recvJSON, runLangServer, serve)
 import qualified LSP.Network.Logging           as Log
 import qualified LSP.Network.Types             as T
-import           LSP.Network.Utils             (untilM_)
+import           LSP.Network.Utils             (retry, textShow, untilM_)
 
 -------------------------------------------------------------------------------
 
@@ -69,16 +70,32 @@ clientIn hin sock m_initBS = do
                return True
   where
     onRecv bs = do
-      Log.logDebug $ "ClientIn: " <> Text.decodeUtf8 bs
-      liftIO $ BS.hPut hin bs
+      isOpen <- liftIO $ IO.hIsOpen hin
+      isWritable <- liftIO $ IO.hIsWritable hin
+      when (isOpen && isWritable) $ do
+        Log.logDebug $ "ClientIn: " <> Text.decodeUtf8 bs
+        liftIO $ BS.hPut hin bs
     {-# INLINE onRecv #-}
 
 serverOut :: Handle -> Socket -> T.ServerApp ()
 serverOut hout sock = do
-  untilM_ (liftIO $ BS.hGetSome hout defaultChunkSize) $ \sout ->
-    if BS.null sout
-       then do Log.logInfo $ "Reach EOF."
-               return False
-       else do Log.logDebug $ "ServerReply: " <> Text.decodeUtf8 sout
-               liftIO $ sendAll sock sout
-               return True
+  isOpen <- liftIO $ IO.hIsOpen hout
+  when isOpen go
+  where
+    tryRead = do
+      isReadable <- liftIO $ IO.hIsReadable hout
+      Log.logDebug $ "Is server command stdout readable: " <> textShow isReadable
+      m_rs <- liftIO $ retry (BS.hGetSome hout defaultChunkSize)
+                             (not . BS.null) 10 (10^(6 :: Int))
+                             -- try read 10 times for every 1 second.
+      case m_rs of
+        Nothing -> return BS.empty
+        Just rs -> return rs
+
+    go = untilM_ tryRead $ \sout ->
+      if BS.null sout
+         then do Log.logInfo $ "Reach EOF."
+                 return False
+         else do Log.logDebug $ "ServerReply: " <> Text.decodeUtf8 sout
+                 liftIO $ sendAll sock sout
+                 return True
--- src/LSP/Network/Utils.hs
+++ src/LSP/Network/Utils.hs
@@ -1,7 +1,14 @@
 module LSP.Network.Utils
   ( untilM_
+  , retry
   , toMaybe
+  , textShow
   ) where
+
+import           Control.Concurrent     (threadDelay)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
+import qualified Data.Text              as Text
+import           Numeric.Natural        (Natural)
 
 -- | Run action until prediction is false.
 untilM_ :: Monad m => m a -> (a -> m Bool) -> m ()
@@ -11,6 +18,17 @@ untilM_ m p = go
       r <- m >>= p
       if r then go else pure ()
 
+retry :: MonadIO m => m a -> (a -> Bool) -> Natural -> Int -> m (Maybe a)
+retry m cond maxn delay
+  | maxn <= 0 = pure Nothing
+  | otherwise = m >>= \r -> if cond r then pure (Just r)
+                                      else do liftIO $ threadDelay delay
+                                              retry m cond (maxn - 1) delay
+
 toMaybe :: Bool -> a -> Maybe a
 toMaybe p x = if p then Just x else Nothing
 {-# INLINE toMaybe #-}
+
+textShow :: Show a => a -> Text.Text
+textShow = Text.pack . show
+{-# INLINE textShow #-}
